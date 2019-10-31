b4321c56c48208d84615a1873004dd5e55a0fd2d
Veltouille <julienstathos@orange.fr>
2019-10-31 15:59:46 +0100

    4 files modified

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  appareilName = "P2020"
+  @Input() appareilName: string;
   ammo: string = 'EMPTY';
 
   constructor() { }
