b4840b9548d95f542903a26393f727ccca2a208b
Veltouille <julienstathos@orange.fr>
2019-11-02 21:39:04 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
     setTimeout(
@@ -17,6 +20,28 @@ export class AppComponent {
 
   name = 'Angular';
   title = "Apex Stathos";
+  secondes: number;
+  counterSub: Subscription;
 
   constructor() { }
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+
+    this.counterSub = counter.subscribe(
+     (value) => {
+       this.secondes = value;
+     },
+     (error) => {
+      console.log("An error occured : " + error);
+     },
+     () => {
+       console.log("Complete !");
+     }
+    )
+  }
+
+  ngOnDestroy() {
+    this.counterSub.unsubscribe();
+  }
 }
