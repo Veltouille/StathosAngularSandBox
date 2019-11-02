b5e7fdc0dab4d8be31c7512432a645791ba1f7f9
Veltouille <julienstathos@orange.fr>
2019-11-02 16:44:20 +0100

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,20 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOFourComponent } from './four-o-four/four-o-four.component';
 
 const appRoutes: Routes = [
   { path: 'appareils', component: AppareilViewComponent },
   { path: 'appareils/:id', component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
-  { path: '', component: AppareilViewComponent }
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOFourComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  declarations: [ AppComponent, HelloComponent, MonPremierComponent, AppareilComponent, AuthComponent, AppareilViewComponent, SingleAppareilComponent ],
+  declarations: [ AppComponent, HelloComponent, MonPremierComponent, AppareilComponent, AuthComponent, AppareilViewComponent, SingleAppareilComponent, FourOFourComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AppareilService, AuthService]
 })
